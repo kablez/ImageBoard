f67faa5ffe4372e4558af736bb65a18684b84c93
Kablez <marco-sousa95@hotmail.com>
2019-02-11 22:45:51 +0000

    Update menuLoggedIn.php

--- public_html/menus/menuLoggedIn.php
+++ public_html/menus/menuLoggedIn.php
@@ -1,5 +1,5 @@
 <header class="header" style="">
-    <a class="menuOption" href="https://weebguy1test.000webhostapp.com/">Home</a>
+    <a class="menuOption" href="https://weebguy1test.000webhostapp.com/">Home2</a>
     <a class="menuOption" href="https://weebguy1test.000webhostapp.com/upload/">Upload</a>
     <a class="menuOption" href="https://weebguy1test.000webhostapp.com/profile/">Profile</a>
     <a class="menuOption" href="https://weebguy1test.000webhostapp.com/php/logout.php">Logout</a>
